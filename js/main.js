bf6b8574d62ec6e2316d4c4fa3fa0037eb0952b7
Tomislav <tomca32@Enterlace.(none)>
2013-06-03 17:42:13 -0400

    Centered map, gates

--- js/main.js
+++ js/main.js
@@ -11,17 +11,37 @@ var requestAnimFrame = (function(){
 
 var map = {
 	width:28,
-	height:10,
+	height:31,
 	data: [
 	"XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
 	"XooooooooooooXXooooooooooooX", 
-	"XoXXXXoXXXXXoXXoXXXXXoXXXXoX", 
+	"XOXXXXoXXXXXoXXoXXXXXoXXXXOX", 
 	"XoXXXXoXXXXXoXXoXXXXXoXXXXoX", 
 	"XooooooooooooooooooooooooooX", 
 	"XoXXXXoXXoXXXXXXXXoXXoXXXXoX", 
+	"XoXXXXoXXoXXXXXXXXoXXoXXXXoX",
 	"XooooooXXooooXXooooXXooooooX", 
-	"XXXXXXoXXXXXoXXoXXXXXoXXXXXX", 
-	"X........X.................X", 
+	"XXXXXXoXXXXX.XX.XXXXXoXXXXXX", 
+	".....XoXXXXX.XX.XXXXXoX.....",
+	".....XoXX..........XXoX.....",
+	".....XoXX.XXXGGXXX.XXoX.....",
+	"XXXXXXoXX.X......X.XXoXXXXXX",
+	"......o...X......X...o......",
+	"XXXXXXoXX.X......X.XXoXXXXXX",
+	".....XoXX.XXXXXXXX.XXoX.....",
+	".....XoXX..........XXoX.....",
+	".....XoXX.XXXXXXXX.XXoX.....",
+	"XXXXXXoXX.XXXXXXXX.XXoXXXXXX",
+	"XooooooooooooXXooooooooooooX",
+	"XoXXXXoXXXXXoXXoXXXXXoXXXXoX",
+	"XoXXXXoXXXXXoXXoXXXXXoXXXXoX",
+	"XOooXXooooooo..oooooooXXooOX",
+	"XXXoXXoXXoXXXXXXXXoXXoXXoXXX",
+	"XXXoXXoXXoXXXXXXXXoXXoXXoXXX",
+	"XooooooXXooooXXooooXXooooooX",
+	"XoXXXXXXXXXXoXXoXXXXXXXXXXoX",
+	"XoXXXXXXXXXXoXXoXXXXXXXXXXoX",
+	"xooooooooooooooooooooooooooX",
 	"XXXXXXXXXXXXXXXXXXXXXXXXXXXX"]
 };
 //Maze dimensions 28 x 31
@@ -38,24 +58,32 @@ function getTileSize(map) {
 	var cH = $('#gameCanvas').innerHeight();
 	if (cW/map.width<cH/map.height){
 		console.log(cW/map.width);
-		return cW/map.width;
+		return Math.floor(cW/map.width);
 	}
 	console.log(cW);
-	return cH/map.height;
+	return Math.floor(cH/map.height);
 }
 
 var tileSize = getTileSize(map);
 console.log(tileSize);
 var ctx = document.getElementById('gameCanvas').getContext('2d');
-function Tile (tileInfo) {
+function Tile (tileInfo,posX,posY) {
 	this.type = tileInfo;
+	this.posX = posX;
+	this.posY = posY;
 }
 
+function getStartX(w){
+	return ($('#gameCanvas').innerWidth() - w*tileSize)/2;
+}
+function getStartY(h){
+	return ($('#gameCanvas').innerHeight() - h*tileSize)/2;
+}
 
 function parseMap(map){
 	var world = {
-		height:  map.height,
-		width:  map.width,
+		height:  map.data.length,
+		width:  map.data[0].length,
 		data: []
 	};
 	var newTile, newLine;
@@ -70,19 +98,23 @@ function parseMap(map){
 
 			switch (tile) {
 				case "X":
-				newTile = new Tile ("closed");
+				newTile = new Tile ("wall", j, i);
 				break;
 
 				case ".":
-				newTile = new Tile ("open");
+				newTile = new Tile ("open", j, i);
 				break;	
 
 				case "o":
-				newTile = new Tile ("pellet");
+				newTile = new Tile ("pellet", j, i);
 				break;
 
 				case "O":
-				newTile = new Tile ("booster");
+				newTile = new Tile ("booster", j ,i);
+				break;
+
+				case "G":
+				newTile = new Tile("gate",j,i);
 				break;
 			}
 			newLine.push(newTile);
@@ -92,15 +124,15 @@ function parseMap(map){
 	return world;
 }
 
-function drawClosedTile(x,y){
+function drawWallTile(x,y){
 	ctx.fillStyle = "rgb(0,34,255)";
 	ctx.strokeStyle = "rgb(0,34,255)";
 	ctx.fillRect(x,y,tileSize,tileSize);
 }
 
 function drawPelletTile(x,y){
-	x = (x+tileSize/2);
-	y = (y+tileSize/2);
+	x = x+tileSize/2;
+	y = y+tileSize/2;
 	console.log(x,y);
 	ctx.fillStyle= "rgb(255,255,0)";
 	ctx.beginPath();
@@ -110,8 +142,8 @@ function drawPelletTile(x,y){
 }
 
 function drawBoosterTile(x,y){
-	x = (x+tileSize/2);
-	y = (y+tileSize/2);
+	x = x+tileSize/2;
+	y = y+tileSize/2;
 	console.log(x,y);
 	ctx.fillStyle= "rgb(255,60,0)";
 	ctx.beginPath();
@@ -120,21 +152,26 @@ function drawBoosterTile(x,y){
 	ctx.fill();	
 }
 
+function drawGateTile(x,y){
+	ctx.fillStyle = "rgb(255,255,255)";
+	ctx.fillRect(x,y,tileSize,tileSize);	
+}
+
 function drawWorld(world) {
 	var x,y, row;
 	worldL = world.data.length;
 	for (var i = 0; i < worldL; i++){
-		y = i*tileSize;
+		y = getStartY(world.height)+i*tileSize;
 		row = world.data[i].length;
 		for (var j = 0; j < row; j++) {
-			x = j * tileSize;
+			x = getStartX(world.width) + j * tileSize;
 			var tile = world.data[i][j];
 			switch (tile.type) {
 				case "open":
 				break;
 
-				case "closed":
-				drawClosedTile(x,y);
+				case "wall":
+				drawWallTile(x,y);
 				break;
 
 				case "pellet":
@@ -144,6 +181,9 @@ function drawWorld(world) {
 				case "booster":
 				drawBoosterTile(x,y);
 				break;
+
+				case "gate":
+				drawGateTile(x,y);
 
 			}
 		}
